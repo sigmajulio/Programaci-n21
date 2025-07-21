0bee8333a5cc8a0e914961b42b99110938900207
sigmajulio <rvuturaf2@gmail.com>
2025-07-21 10:35:30 -0600

    Hola inge :))))))))))))))))

---
+++ nigga.js
@@ -0,0 +1,16 @@
+//EJER 1
+let base = 3
+let altura = 3
+let areaTriangulo = (base * altura)/2
+console.log(`el area del triangulo es ${areaTriangulo}`)
+
+//EJER 2
+let n1 = 1000000 //Salario de Julio diario
+let n2 = 0.001 //Salario de Olmert anual
+let comparison =  (n1 > n2 ) ? "Julio" : "Olmert"
+console.log(`¿Quién está facturando más? ${comparison}`) 
+
+//EJER 3
+let n = -3
+let evaluation = (n > 10 && n < 20) ? "Si UwU" : "No UwU"
+console.log(`el número ${n} está dentro del intérvalo [10, 20]? ${evaluation}`) 
---
+++ operadores.js
@@ -0,0 +1,40 @@
+//OPERADORES ARITMÉTICOS
+let a = 15;
+let b = 4;
+
+suma = a+b;
+console.log(`La suma de a + b es: ${
+}`)
+
+
+//OPERADORES DE COMPARACIÓN
+
+console.log("¿Edad 1 > Edad 2?", edad1 > edad);
+console.log("¿Edad 1 < Edad 2?", edad1 > edad);
+console.log("¿Edad 1 == Edad 2?", edad1 > edad);
+console.log("¿Edad 1 != Edad 2?", edad1 > edad);
+console.log("¿Edad 1 >= Edad 2?", edad1 > edad);
+console.log("¿Edad 1 <= Edad 2?", edad1 > edad);
+console.log("¿Edad 1 > Edad 2?", edad1 > edad);
+
+//OPEADORES DE ASIGNACION X
+let puntos =0;
+
+puntos = puntos + 10;
+console.log(`el puntaje final es ${puntos}`)
+puntos += 10
+
+puntos -= 5;
+puntos *= 2;
+
+//OPERADORES LÓGICOS X
+let tieneTarea = false;
+let estudio = true;
+
+let puedeSalir = !tieneTarea && estudio;
+console.log(`¿Puede salir a jugar? ${puedeSalir}`)
+
+//OPERADOR TERNARIO X
+let numero =9;
+
+let resultado = (numero % 2 === 0 ) ? "Par" : "Impar"
